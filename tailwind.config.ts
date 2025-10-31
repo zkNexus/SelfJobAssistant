db67c58c7ef29773a93794ec8c5b21d834244eda
Julio M Cruz <julio.cruz@eb-ms.net>
2025-10-30 18:03:48 -0700

    Update framework

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/selfx402-pay-widget/**/*.{js,mjs}",
   ],
   theme: {
     extend: {
